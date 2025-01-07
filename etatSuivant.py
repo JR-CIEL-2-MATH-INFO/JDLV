196f4fd6613f95049d1e5e1a03a63488aae89710
madaniJr <95218279+madaniJr@users.noreply.github.com>
2025-01-07 11:07:04 +0100

    Update etatSuivant.py a completer

--- etatSuivant.py
+++ etatSuivant.py
@@ -1,21 +1,36 @@
 def etatSuivant(g, i, j):
     # Récupérer les voisins de la cellule (i, j)
-    voisins = positionsCasesVoisines(g, i, j)
+    #voisins = positionsCasesVoisines(....., ......, ........)
     
     # Compter le nombre de voisins vivants
-    voisins_vivants = nombreVoisinesVivantes(g, i, j)
+    #voisins_vivants = ................
     
     # Récupérer l'état actuel de la cellule (i, j)
-    etat_actuel = g[i][j]
+    # etat_actuel = ..........
     
     # Appliquer les règles du Jeu de la Vie
     if etat_actuel:  # Si la cellule est vivante
-        if voisins_vivants == 2 or voisins_vivants == 3:
-            return True  # Reste vivante
+       # if voisins_vivants == .... or voisins_vivants == ......:
+            return .....  # Reste vivante
         else:
-            return False  # Meurt
+            return .....  # Meurt
     else:  # Si la cellule est morte
-        if voisins_vivants == 3:
-            return True  # Devient vivante
+       # if voisins_vivants == .....:
+            return .....  # Devient vivante
         else:
-            return False  # Reste morte
+            return .....  # Reste morte
+
+# Exemple d'utilisation
+grille = [
+    [False, False, False, False, False],
+    [False, True,  True,  True,  False],
+    [False, False, True,  False, False],
+    [False, False, False, False, False],
+    [False, False, False, False, False],
+]
+
+# Test de la fonction
+# À compléter : choisir une cellule pour tester l'état suivant
+# etat_suivant = etatSuivant(grille, ..., ...)
+# print(etat_suivant)
+
