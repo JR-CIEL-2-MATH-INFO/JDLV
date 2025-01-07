c7ce4f174a37840482dae39f0506a8627917b784
madaniJr <95218279+madaniJr@users.noreply.github.com>
2025-01-07 10:38:18 +0100

    Update JDLVComplet.py

--- JDLVComplet.py
+++ JDLVComplet.py
@@ -39,7 +39,22 @@ def afficherGrille(grille):
     # Lancer la boucle principale Tkinter
     fenetre.mainloop()
 
+def positionsCasesVoisines(g, i, j):
+    voisins = []
+    lignes = len(g)
+    colonnes = len(g[0])
 
+    # Liste des directions possibles : haut, bas, gauche, droite, diagonales
+    directions = [(-1, 0), (1, 0), (0, -1), (0, 1), (-1, -1), (-1, 1), (1, -1), (1, 1)]
+
+    for di, dj in directions:
+        ni, nj = i + di, j + dj
+        # Vérifier si la nouvelle position est dans les limites de la grille
+        if 0 <= ni < lignes and 0 <= nj < colonnes:
+            voisins.append((ni, nj))
+    
+    return voisins
+    
 def nombreVoisinesVivantes(g, i, j):
     # Appeler la fonction positionsCasesVoisines pour obtenir les voisins de la cellule (i, j)
     voisins = positionsCasesVoisines(g, i, j)
